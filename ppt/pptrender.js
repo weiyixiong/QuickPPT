283cc1d632d3775e28a4c1315eb441a5bf41d957
weiyixiong <542111388@qq.com>
2016-02-19 16:33:36 +0800

    整理代码

--- ppt/pptrender.js
+++ ppt/pptrender.js
@@ -10,7 +10,14 @@ PPTRender.prototype.render=function render(){
     var baseSvg = d3.select("#tree-container").append("svg")
     .attr("width", viewerWidth)
     .attr("height", viewerHeight)
-    .attr("class", "overlay")
+    .attr("class", "overlay");
+
+    baseSvg.append('rect').attr('x',838200/6350).attr('y', 365125/6350).attr('width',10515600/6350).attr('height',1325563/6350).attr('style', 'fill:blue');
+    //x="20" y="20" width="250" height="250"
+ 	baseSvg.append('rect').attr('x',838200/6350).attr('y', 365125/6350).attr('width',10515600/6350).attr('height',1325563/6350).attr('style', 'fill:blue');
+    //x="20" y="20" width="250" height="250"
+
+
 
      
 }
